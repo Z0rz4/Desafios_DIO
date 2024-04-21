ebfebfea7d3d25d3c07bfffd93425046dabf02d2
Z0rz4 <kalelzorzanello4@gmail.com>
2024-04-21 18:08:05 -0300

    segundo commit de teste

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello World")
+console.log("Hello World");
+let array = ["n","a","m","e"];
+let contador = 0;
+
+
+while(contador < array.length){
+    console.log(array[contador]);
+    contador++;
+}
